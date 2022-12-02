bd6fe932fa1606033895e7132cad50e1877b1d1c
Kadir Boylu <kadir.boylu@outlook.com>
2022-12-02 15:42:59 +0300

    Add icon to header

--- home/src/components/Header.jsx
+++ home/src/components/Header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="p-5 bg-gray-700 text-white text-2xl font-bold">
-      Game Store
+    <header className="p-5 bg-gray-700 text-white flex items-center gap-2">
+      <i className="ri-gamepad-line text-3xl"></i>
+      <h1 className="font-bold text-2xl">Game Store</h1>
     </header>
   );
 }
