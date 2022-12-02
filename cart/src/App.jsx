2e883d202add1daf4fa3f20eb657c2a5111df73c
Kadir Boylu <kadir.boylu@outlook.com>
2022-12-02 13:27:41 +0300

    create login component

--- cart/src/App.jsx
+++ cart/src/App.jsx
@@ -7,12 +7,14 @@ import "remixicon/fonts/remixicon.css";
 import Header from "home/components/Header";
 import Footer from "home/components/Footer";
 import CartContent from "./components/CartContent";
+import Login from "./components/Login";
 
 const App = () => (
   <div className="mx-auto max-w-6xl">
     <Header />
     <div className="content">
       <CartContent />
+      <Login />
     </div>
     <Footer />
   </div>
--- cart/src/components/CartContent.jsx
+++ cart/src/components/CartContent.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import { login, jwt } from "../cart";
+import Login from "./Login";
 
 export default function CartContent() {
   const [token, setToken] = useState("");
+
+  useEffect(() => {
+    return jwt.subscribe((val) => setToken(val ?? ""));
+  }, []);
 
   return <div>JWT: {token}</div>;
 }
