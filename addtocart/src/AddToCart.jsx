db470663fc3e5a4c94cf59501d8f882bfb849c89
Kadir Boylu <kadir.boylu@outlook.com>
2022-12-03 16:32:46 +0300

    create addtocart project

---
+++ addtocart/src/AddToCart.jsx
@@ -0,0 +1,21 @@
+import { createEffect, createSignal, Show } from "solid-js";
+
+import { jwt, addToCart } from "cart/cart";
+
+export default ({ id }) => {
+  const [loggedIn, setLoggedIn] = createSignal(false);
+
+  createEffect(() => {
+    return jwt.subscribe((jwt) => {
+      setLoggedIn(!!jwt);
+    });
+  });
+
+  return (
+    <Show when={loggedIn()}>
+      <button onClick={() => addToCart(id)} class="btn danger">
+        Add to Cart
+      </button>
+    </Show>
+  );
+};
